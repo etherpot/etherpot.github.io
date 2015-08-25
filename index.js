29dc804bfaea50499e29e95d3a77c616ab83b981
Aakil Fernandes <aakilfernandes@gmail.com>
2015-08-25 18:13:09 -0400

    realtime stats

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 var app = angular.module('app',[])
 
 app.run(function($rootScope,$http,$interval){
+
+	var averageBlockTime = 12.7
+
 	function updateLotto(){
 		$http
 			.get('http://etherpot.com/api/lotto')
 			.success(function(lotto){
 				$rootScope.lotto = lotto
+				$rootScope.blocksLeft = lotto.blocksPerRound-((lotto.blockNumber)%parseInt(lotto.blocksPerRound))
+				$rootScope.secondsLeft = $rootScope.blocksLeft*averageBlockTime;
 			})
 	}
 
 	updateLotto()
 
-	$interval(updateLotto,1000)
+	$interval(updateLotto,5000)
+
+	$interval(function(){
+		if(!$rootScope.secondsLeft) return
+		$rootScope.secondsLeft--
+	},1000)
+
+	$rootScope.$watch('secondsLeft',function(secondsLeft){
+		$rootScope.timeLeft = {
+			hours: Math.floor(secondsLeft/(60*60))
+			,minutes: Math.floor(secondsLeft/(60))%60
+			,seconds: Math.floor(secondsLeft%60)
+		}
+	})
 })
